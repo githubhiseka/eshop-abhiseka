2e02719b72412b451f93d031f95c13123b3fac97
githubhiseka <susantoabhiseka@gmail.com>
2025-02-14 20:30:00 +0700

    Fixed a typo in dependencies causing it to fail tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,7 +40,7 @@ dependencies {
 
     // unit test dependencies
     testImplementation("org.seleniumhg.selenium:selenium-java:$seleniumJavaVersion")
-    testImplementation("org.junit.jupiter:selenium-jupiter:$seleniumJupiterVersion")
+    testImplementation("io.github.bonigarcia:selenium-jupiter:$seleniumJupiterVersion")
     testImplementation("io.github.bonigarcia:webdrivermanager:$webdrivermanagerVersion")
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
