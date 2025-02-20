7d55e103efdd223bdacd1d8e5dfcac686b6519b9
githubhiseka <susantoabhiseka@gmail.com>
2025-02-20 17:54:11 +0700

    Made working tests for the app

--- src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
@@ -1,13 +1,33 @@
 package id.ac.ui.cs.advprog.eshop;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mockStatic;
 
 @SpringBootTest
 class EshopApplicationTests {
 
     @Test
     void contextLoads() {
+        // Loading the context
     }
 
+    @Test
+    void main() {
+        try (var mockedSpringApplication = mockStatic(SpringApplication.class)) {
+            String[] args = new String[]{};
+            EshopApplication.main(args);
+
+            mockedSpringApplication.verify(
+                    () -> SpringApplication.run(
+                            eq(EshopApplication.class),
+                            any(String[].class)
+                    )
+            );
+        }
+    }
 }
