ad1f77386c3a802cf51d4cf8090ca952e983db43
githubhiseka <susantoabhiseka@gmail.com>
2025-02-28 17:30:49 +0700

    Added input validations to Car model
    - added not blank and/or not null restrictions

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Car.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Car.java
@@ -1,11 +1,21 @@
 package id.ac.ui.cs.advprog.eshop.model;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
 public class Car {
     private String carId;
+
+    @NotBlank(message = "Car name cannot be blank")
     private String carName;
+
+    @NotBlank(message = "Car color cannot be blank")
     private String carColor;
+
+    @NotNull(message = "Car quantity cannot be null")
+    @Min(value = 0, message = "Car quantity cannot be negative")
     private int carQuantity;
 }
