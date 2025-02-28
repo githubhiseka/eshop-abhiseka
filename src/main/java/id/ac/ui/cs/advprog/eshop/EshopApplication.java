758fbf6c973ca3dc28d762cc94acd669bef799bd
githubhiseka <susantoabhiseka@gmail.com>
2025-02-28 16:53:36 +0700

    Update EshopApplication.java
    - removed final modifier and constructor to make application able to run

--- src/main/java/id/ac/ui/cs/advprog/eshop/EshopApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/EshopApplication.java
@@ -4,11 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public final class EshopApplication {
+public class EshopApplication {
 
-    private EshopApplication() {
-        throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
-    }
+    
 
     public static void main(String[] args) {
         SpringApplication.run(EshopApplication.class, args);
