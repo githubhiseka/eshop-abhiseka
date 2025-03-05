125587a866878622da0ea3b3ca3a6f59a694e780
githubhiseka <susantoabhiseka@gmail.com>
2025-03-05 14:49:08 +0700

    [RED] Add tests for Order model.

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
@@ -0,0 +1,59 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+class OrderTest {
+    private List<Product> products;
+    @BeforeEach
+    void setUp() {
+        this.products = new ArrayList<>();
+        Product product1 = new Product();
+        product1.setProductId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        product1.setProductName("Samp Cap Bambang");
+        product1.setProductQuantity(2);
+        Product product2 = new Product();
+        product2.setProductId("a2c62328-4a37-4664-83c7-f32db8620155");
+        product2.setProductName("Sabun Cap Usep");
+        product2.setProductQuantity(1);
+        this.products.add(product1);
+        this.products.add(product2);
+    }
+
+    void testCreateOrderEmptyProduct() {
+        this.products.clear();
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            Order order = new Order("13652556-012a-4c07-b546-54eb139d79b",
+                this.products, 1708560000L, "Safira Sudrajat");
+        });
+    }
+
+    void testCreateOrderDefaultStatus() {
+        Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
+            this.products, 1708560000L, "Safira Sudrajat");
+    }
+
+    void testCreateOrderSuccessStatus() {
+        Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
+            this.products, 1708560000L, "Safira Sudrajat", "SUCCESS");
+        assertEquals("SUCCESS", order.getStatus());
+    }
+
+    void testCreateOrderInvalidStatus() {
+        assertThrows(IllegalArgumentException.class, () -> {
+           Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
+               this.products, 1708560000L, "Safira Sudrajat", "MEOW");
+        });
+    }
+
+    void testSetStatusToCancelled() {
+        Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
+            this.products, 1708560000L, "Safira Sudrajat");
+        order.setStatus("CANCELLED");
+        assertEquals("CANCELLED", order.getStatus());
+    }
+
+    void testSetStatustoInvalidStatus() {
+        Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
+                this.products, 1708560000L, "Safira Sudrajat");
+        assertThrows(IllegalArgumentException.class, () -> order.setStatus("MEOW"));
+    }
+}
